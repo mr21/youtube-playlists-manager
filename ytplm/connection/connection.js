9f5f98e53d08a4e577d7bc5f3c4e79d673eb3d52
Thomas Tortorini <thomastortorini@gmail.com>
2014-09-24 07:46:22 +0200

    JS/CSS, connection, plus besoin de bodyStatus

--- ytplm/connection/connection.js
+++ ytplm/connection/connection.js
@@ -3,24 +3,15 @@ ytplm.connection = {
 		client_id: '514381834459-gr3dgmesev8jflg5364piutqb6alb35b.apps.googleusercontent.com',
 		scope: ['https://www.googleapis.com/auth/youtube']
 	},
-	bodyStatus: function(status) {
-		document.body.setAttribute('connected', status);
-	},
 	logout: function() {
 		gapi.auth.signOut();
-		this.bodyStatus(0);
 	},
 	login: function(callback) {
 		var self = this;
 		gapi.auth.authorize(this.id, function(authResult) {
 			if (authResult.status.signed_in) {
-				gapi.client.load('youtube', 'v3', function() {
-					self.bodyStatus(1);
-					if (callback)
-						callback();
-				});
+				gapi.client.load('youtube', 'v3', callback);
 			} else {
-				self.bodyStatus(0);
 				console.log('Connection failed... :(');
 			}
 		});
