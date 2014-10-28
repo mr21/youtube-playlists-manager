e372f2353b9b83c11b08ebc51f1e83ebb5f56927
Thomas Tortorini <thomastortorini@gmail.com>
2014-10-28 17:24:04 +0100

    JS, gapi, update

--- ytplm/main.js
+++ ytplm/main.js
@@ -7,7 +7,7 @@ var ytplm = {};
 
 window.gapi_onload = function() {
 	gapi.client.setApiKey(ytplm.connection.apiKey);
-	gapi.client.load('youtube', 'v3', function() {
+	gapi.client.load('youtube', 'v3').then(function() {
 		ytplm.tabs.init();
 	});
 };
